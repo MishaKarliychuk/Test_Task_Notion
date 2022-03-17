20417de01f7c7b1f3705ff861f9d30c462096fe8
MishaKarliychuk <mishakarliyshuk@gmail.com>
2022-03-17 20:44:25 +0200

    added req

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def get_data():
     # subprocess.Popen(['python', '/home/misha/PycharmProjects/etc-test/script.py'])
     return "<h1>Успешно запущено!</h1>"
 
-if __name__ == '__main__':
-    app.debug = True
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+app.debug = True
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
